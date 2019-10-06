0f31a07b1ff796089c2c020b21b6f142209f0174
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2019-10-06 18:16:40 +0200

    Rename virtualhost name

--- provisioning/containers/apache/tasks/setup-virtual-host.sh
+++ provisioning/containers/apache/tasks/setup-virtual-host.sh
@@ -2,7 +2,7 @@
 
 cd /etc/apache2
 
-ln -s /templates/sites-enabled/press-review.conf /etc/apache2/sites-available
+ln -s /templates/sites-enabled/devobs.conf /etc/apache2/sites-available
 
 # Disable default virtual host
 a2dissite 000-default.conf
@@ -17,7 +17,7 @@ a2enmod headers
 a2enmod http2
 
 cd /etc/apache2
-ln -s `pwd`/sites-available/press-review.conf `pwd`/sites-enabled
+ln -s `pwd`/sites-available/devobs.conf `pwd`/sites-enabled
 
 rm `pwd`/mods-enabled/deflate.conf
 ln -s /templates/mods-enabled/deflate.conf `pwd`/mods-enabled
