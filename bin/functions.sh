334e87e3748dbc2f5a3d9062469ce9f97ab7b361
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2019-10-06 18:11:12 +0200

    Add function to set ACL

--- bin/functions.sh
+++ bin/functions.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# 2019-10-06 - Notes about running Apache container
+#
+# ```
+# make build-php-container
+# make build-apache-container
+#
+# # RabbitMQ startup has to be complete
+# # when generating cached application configuration
+# make run-rabbitmq
+#
+# # Install PHP vendors
+# make install-php-dependencies
+#
+# make run-apache-container
+#
+# # Set ACL
+# make set-acl
+# ```
+#
 # 2019-06-27 - Notes
 #
 # - Ensure environment variable PROJECT_DIR has been declared
@@ -308,19 +327,27 @@ function diff_schema {
 # https://symfony.com/doc/3.4/setup/file_permissions.html#using-acl-on-a-system-that-supports-setfacl-linux-bsd
 function set_acl {
     local command
-    command="apt install -y acl &&
-    cd /var/www/devobs &&
-    ! test -d ./app/cache && mkdir -p ./app/cache || echo 'cache dir exists'
-    ! test -d ./app/logs && mkdir -p ./app/logs || echo 'logs dir exists'
-    ! test -d ./app/var && mkdir -p ./app/var || echo 'var dir exists'
-    setfacl -dR -m u:jenkins:rwX -m u:1000:rwX /var/www/devobs/app/cache &&
-    setfacl -R -m u:jenkins:rwX -m u:1000:rwX /var/www/devobs/app/cache &&
-    setfacl -dR -m u:jenkins:rwX -m u:1000:rwX /var/www/devobs/app/logs &&
-    setfacl -R -m u:jenkins:rwX -m u:1000:rwX /var/www/devobs/app/logs &&
-    setfacl -dR -m u:jenkins:rwX -m u:1000:rwX /var/www/devobs/app/var &&
-    setfacl -R -m u:jenkins:rwX -m u:1000:rwX /var/www/devobs/app/var &&
-    chmod -R 0775 /var/www/devobs"
-    docker run apache /bin/bash -c "${command}"
+    local owner
+    local project_dir
+
+    project_dir="$(get_project_dir)"
+    owner=jenkins
+
+    command='apt install -y acl &&
+    cd "'${project_dir}'" &&
+    echo `pwd` && ls -lahtr ./ &&
+    (! test -d ./app/cache && mkdir -p ./app/cache) || (ls ./app && echo "cache dir exists") &&
+    (! test -d ./app/logs && mkdir -p ./app/logs) || (ls ./app && echo "logs dir exists") &&
+    (! test -d ./app/var && mkdir -p ./app/var) || (ls ./app && echo "var dir exists") &&
+    setfacl -dR -m u:'"${owner}"':rwX -m u:1000:rwX "'${project_dir}'"/app/cache &&
+    setfacl -R -m u:'"${owner}"':rwX -m u:1000:rwX "'${project_dir}'"/app/cache &&
+    setfacl -dR -m u:'"${owner}"':rwX -m u:1000:rwX "'${project_dir}'"/app/logs &&
+    setfacl -R -m u:'"${owner}"':rwX -m u:1000:rwX "'${project_dir}'"/app/logs &&
+    setfacl -dR -m u:'"${owner}"':rwX -m u:1000:rwX "'${project_dir}'"/app/var &&
+    setfacl -R -m u:'"${owner}"':rwX -m u:1000:rwX "'${project_dir}'"/app/var &&
+    chown -R 1000:'"${owner}"' "'${project_dir}'/app" &&
+    chmod -R 0775 "'${project_dir}'/app"'
+    docker exec -d apache /bin/bash -c "${command}"
 }
 
 # In production, export the *appropriate* environment variable (contains "_accepted_") to migrate a schema
