7032eea8daa4951512ee8ae9d5c2faaafbd8bc45
Thierry Marianne <thierry@marianne.io>
2020-12-22 19:36:46 +0100

    Fix implementation of search params interface

--- src/Twitter/Infrastructure/Http/SearchParams.php
+++ src/Twitter/Infrastructure/Http/SearchParams.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Twitter\Infrastructure\Http;
 
+use App\NewsReview\Domain\Repository\SearchParamsInterface;
 use Symfony\Component\HttpFoundation\Request;
 use function array_key_exists;
 use function array_keys;
@@ -16,7 +17,7 @@ use function serialize;
 use function sha1;
 use function trim;
 
-class SearchParams
+class SearchParams implements SearchParamsInterface
 {
     public const PARAM_AGGREGATE_IDS = 'aggregateIds';
 
@@ -194,5 +195,15 @@ class SearchParams
     public function getFingerprint()
     {
         return sha1(serialize($this));
+    }
+
+    /**
+     * @param string $name
+     * @param        $value
+     * @return bool
+     */
+    public function paramIs(string $name, $value): bool
+    {
+        return $this->hasParam($name) && $this->params[$name] === $value;
     }
 }
